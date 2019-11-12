dc4875c8e18da36f965af34379d2e78295a65185
Stuart Carnie <stuart.carnie@gmail.com>
2019-11-11 23:57:03 -0700

    feat: Minimized in-game menu; resolved video sizing issues

--- src/osd/headless/public/oecommon.h
+++ src/osd/headless/public/oecommon.h
@@ -3,3 +3,12 @@
 #ifndef OE_EXPORTED_CLASS
 #define OE_EXPORTED_CLASS     __attribute__((visibility("default")))
 #endif
+
+#ifndef OE_STATIC_ASSERT
+#define OE_STATIC_ASSERT(_condition, ...) static_assert(_condition, "" __VA_ARGS__)
+#endif
+
+#ifndef OE_ENUM_CHECK
+#define OE_ENUM_CHECK(_enum, _src, _c99enumcount) \
+    OE_STATIC_ASSERT(_enum::_src == _enum(_c99enumcount) )
+#endif
